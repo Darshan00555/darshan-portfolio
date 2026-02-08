4da1301caba0a29035c1f584f40b31df4c27bc1d
Darshan00555 <darshan.wkhra1@gmail.com>
2026-02-08 16:57:59 +0530

    chore: increase loader timeout to 1.8s

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1500);
+    }, 1800);
 
     return () => clearTimeout(timer);
   }, []);
