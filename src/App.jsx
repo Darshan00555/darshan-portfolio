33d20041fa99cb17f82a11c730d175d19e76ddf5
Darshan00555 <darshan.wkhra1@gmail.com>
2026-02-08 16:21:05 +0530

    feat: switch to TubelightNavbar with z-index and animation fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Home from './pages/Home';
+import { NavBarDemo } from './components/NavBarDemo';
+
+
 
 function App() {
   return (
     <>
+
+      <NavBarDemo />
       <Home />
     </>
   );
